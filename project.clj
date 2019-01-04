ad19a16ebf6435d137ab409b66541520de817416
Huahai Yang <huahai.yang@gmail.com>
2019-01-04 11:34:01 -0800

    update deps

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject jarohen/chord "0.8.1"
+(defproject juji/chord "0.8.2"
   :description "A library to bridge the gap between CLJ/CLJS, web-sockets and core.async"
   :url "https://github.com/james-henderson/chord.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [http-kit "2.1.19"]
-                 [org.clojure/tools.reader "0.9.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]
+                 [http-kit "2.3.0"]
+                 [org.clojure/tools.reader "1.3.2"]
 
-                 [cheshire "5.5.0"]
+                 [cheshire "5.8.1"]
 
-                 [com.cognitect/transit-clj "0.8.275"]
-                 [com.cognitect/transit-cljs "0.8.220"]
+                 [com.cognitect/transit-clj "0.8.313"]
+                 [com.cognitect/transit-cljs "0.8.256"]
 
                  [org.clojure/data.fressian "0.2.1"]
                  [net.unit8/fressian-cljs "0.2.0"]])
